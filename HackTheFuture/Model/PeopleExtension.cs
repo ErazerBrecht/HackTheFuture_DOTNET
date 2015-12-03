49b9555915fef046f254969c1b3f1ccb52c34621
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-03 15:10:51 +0100

    Added people extension

---
+++ HackTheFuture/Model/PeopleExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackTheFuture
+{
+    public partial class People
+    {
+        public bool Delete { get; set; }
+    }
+}
